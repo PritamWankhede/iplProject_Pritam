2a9b377bc44e8d6b58e3c70f3ca16a381f777053
PritamWankhede <pritamwankhede671@gmail.com>
2024-09-23 23:12:42 +0530

    files uploaded

--- Server/3-ExtraRuns.js
+++ Server/3-ExtraRuns.js
@@ -1,39 +1,40 @@
 
 //3)Extra runs conceded per team in the year 2016
-     function ExtraRunByTeam(matchesData,deliveryData){
-      let seasonData = matchesData.reduce((accumulator,match)=>{
-        let id = match.id;
-        let season = match.season;
-  
-         if(id !== undefined){
-         if(!accumulator[id]){
-            accumulator[id] = season;
-         }
-        }
-         return accumulator;
-      },{});
+function ExtraRunByTeam(matchesData, deliveryData) {
+   let seasonData = {};
+   for (let i = 0; i < matchesData.length; i++) {
+       let match = matchesData[i];
+       let id = match.id;
+       let season = match.season;
 
-     let teamConcededExtraRun = deliveryData.reduce((accumulator,delivery)=>{
-        let bowlingTeam = delivery["bowling_team"];
-        let extraRun = delivery["extra_runs"];
-        let id = delivery["match_id"];
-         let year = seasonData[id];
+       if (id !== undefined) {
+           if (!seasonData[id]) {
+               seasonData[id] = season;
+           }
+       }
+   }
 
-         if(year !== undefined){
-         if(year === 2016){
-            if(!accumulator[year]){
-                accumulator[year] = {} 
-            }
-  
-            if(!accumulator[year][bowlingTeam]){
-                accumulator[year][bowlingTeam] = 0;  
-            }
-            accumulator[year][bowlingTeam] += Number([extraRun]);
-         }
-     }
-       return accumulator;
-     },{});
-     return teamConcededExtraRun;
-     }
-     //console.log(ExtraRunByTeam(matchesData,deliveriesData));
-     module.exports = ExtraRunByTeam; 
+   let teamConcededExtraRun = {};
+   for (let i = 0; i < deliveryData.length; i++) {
+       let delivery = deliveryData[i];
+       let bowlingTeam = delivery["bowling_team"];
+       let extraRun = delivery["extra_runs"];
+       let id = delivery["match_id"];
+       let year = seasonData[id];
+
+       if (year !== undefined && year === 2016) {
+           if (!teamConcededExtraRun[year]) {
+               teamConcededExtraRun[year] = {};
+           }
+           if (!teamConcededExtraRun[year][bowlingTeam]) {
+               teamConcededExtraRun[year][bowlingTeam] = 0;
+           }
+           teamConcededExtraRun[year][bowlingTeam] += Number(extraRun);
+       }
+   }
+
+   return teamConcededExtraRun;
+}
+
+// console.log(ExtraRunByTeam(matchesData, deliveriesData));
+module.exports = ExtraRunByTeam;
